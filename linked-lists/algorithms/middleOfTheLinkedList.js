4b2bd4c2b602bbfbce84eb4e4754b99febc528ad
Mert Eldemir <mert1greatest@gmail.com>
2024-07-06 21:10:30 +0300

    linkedList algorithms last changes

--- linked-lists/algorithms/middleOfTheLinkedList.js
+++ linked-lists/algorithms/middleOfTheLinkedList.js
@@ -2,6 +2,14 @@
 
 // find the middle node of the given linked list
 
+// this is the definition for the LinkedList
+// class LinkedList {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
+
 function middleNode(head) {
   let slow = head;
   let fast = head;
