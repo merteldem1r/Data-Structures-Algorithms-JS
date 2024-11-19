d2e3bb48695204b26bc5a4b42883dfc8087c4f61
Mert Eldemir <mert1greatest@gmail.com>
2024-11-19 21:42:37 +0300

    Merge sort uptadet

--- Algorithms/arrays/MergeSort.js
+++ Algorithms/arrays/MergeSort.js
@@ -1,25 +1,41 @@
 const arr = [38, 27, 43, 3, 9, 82, 10];
 
-function mergeSort(arr) {
-  if (arr.length <= 1) return arr;
+// Example of l, mid, h, i, j:
+//  l        mid             h
+// [1, 2, 6, 12, 17, 25, 43, 52]
+//  i              j
 
-  const mid = Math.floor(arr.length / 2);
-  const left = mergeSort(arr.slice(0, mid));
-  const right = mergeSort(arr.slice(mid));
+function merge(arr, l, mid, h) {
+  const tempArr = new Array(h + 1);
 
-  return merge(left, right);
-}
+  let i = l;
+  let j = mid + 1;
+  let k = l;
 
-function merge(left, right) {
-  let sortedArray = [];
-  while (left.length && right.length) {
-    if (left[0] < right[0]) {
-      sortedArray.push(left.shift());
-    } else {
-      sortedArray.push(right.shift());
-    }
+  while (i <= mid && j <= h) {
+    if (arr[i] < arr[j]) tempArr[k++] = arr[i++];
+    else tempArr[k++] = arr[j++];
   }
-  return sortedArray.concat(left, right);
+
+  for (; i <= mid; ++i) tempArr[k++] = arr[i];
+
+  for (; j <= h; ++j) tempArr[k++] = arr[j];
+
+  for (i = l; i <= h; ++i) {
+    arr[i] = tempArr[i];
+  }
 }
 
-console.log(mergeSort(arr));
+function MergeSort(arr, l, h) {
+  if (l >= h) return;
+
+  const mid = l + Math.floor((h - l) / 2);
+
+  MergeSort(arr, l, mid);
+  MergeSort(arr, mid + 1, h);
+  merge(arr, l, mid, h);
+}
+
+console.log("Before: ", arr);
+console.log(MergeSort(arr, 0, arr.length - 1));
+console.log("After: ", arr);
---
+++ Algorithms/arrays/QuickSort.js
@@ -0,0 +1,9 @@
+const arr = [6, 5, 3, 1, 8, 7, 2, 4];
+
+function partition(arr, l, h) {}
+
+function QuickSort(arr, l, h) {}
+
+console.log("Before: ", arr);
+console.log(QuickSort(arr, 0, arr.length - 1));
+console.log("After: ", arr);
